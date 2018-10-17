74bf5d2221244fb83790e54e4a1d272cb181a035
Jered Dowden <jereddowden@jdow-book.local>
2018-10-17 15:57:54 -0400

    GitHub Service cleanup

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,20 +1,42 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError, forkJoin } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+const GITHUB_API_URL = 'https://api.github.com/users/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
   private username: string;
-  private githubEndPoint = 'https://api.github.com/users/';
 
   constructor(private http: HttpClient) {}
 
-  getUser(username: string) {
-    return this.http.get(`${this.githubEndPoint}${username}`);
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(`GitHub returned ${error.status}, ` + `Error was: ${error.error.message}`);
+    }
+    return throwError(
+      `Whoops! We either couldn't find that user or something went wrong on our end. Please try again later.`
+    );
   }
 
-  getRepos(username: string) {
-    return this.http.get(`${this.githubEndPoint}${username}/repos`);
+  getUser(username: string): Observable<any> {
+    return this.http.get(`${GITHUB_API_URL}${username}`).pipe(catchError(this.handleError));
+  }
+
+  getRepos(username: string): Observable<any> {
+    return this.http.get(`${GITHUB_API_URL}${username}/repos`).pipe(catchError(this.handleError));
+  }
+
+  getStarred(username: string): Observable<any> {
+    return this.http.get(`${GITHUB_API_URL}${username}/starred`).pipe(catchError(this.handleError));
+  }
+
+  getUserReposAndStarred(username: string): Observable<any> {
+    return forkJoin(this.getUser(username), this.getRepos(username), this.getStarred(username));
   }
 }
